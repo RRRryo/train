76a5d983e2c88bf1d35c1afbfde0feece57fe9cf
Liang <liang6872@gmail.com>
2018-06-13 05:46:39 +0800

    second commit

--- src/main/java/Singleton.java
+++ src/main/java/Singleton.java
@@ -4,8 +4,21 @@
 public enum Singleton {
     INSTANCE;
 
+    //是我们要应用单例模式的资源，具体可以表现为网络连接，数据库连接，线程池等等
+    private Object resource;
+
+    Singleton() {
+        //init method here
+        System.out.println("init method");
+
+    }
+
     public void job1() {
         System.out.print("do the job1");
+    }
+
+    public Object getResource() {
+        return resource;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/StringToInteger.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 6/13/2018.
+ */
+public class StringToInteger {
+
+
+}
